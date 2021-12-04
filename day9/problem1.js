1d2a916c8fad0932dbf4059ca5100415dedeb791
Shivam Rajput <shivam.rajput@thoughtworks.com>
2021-12-04 13:20:45 +0530

    solved day 9 problem

---
+++ day9/problem1.js
@@ -0,0 +1,54 @@
+const fs = require('fs')
+
+const parseDistances = data => {
+  return data.map(d => {
+    const [locations, distance] = d.split(' = ')
+    const [location1, location2] = locations.split(' to ')
+    return {locations: [location1, location2], distance: +distance}
+  })
+}
+
+const findAllLocations = (distances) => {
+  return distances.reduce((locations, location) => {
+    if (!locations.includes(location.locations[0])) locations.push(location.locations[0])
+    if (!locations.includes(location.locations[1])) locations.push(location.locations[1])
+    return locations
+  }, []);
+};
+
+const isVisitedBothCities = (visitedCities, locations) => {
+  return visitedCities.includes(locations[0]) && visitedCities.includes(locations[1]);
+};
+
+const findNearestCity = (distances, location, visitedCities) => {
+  return distances.filter(({locations}) => locations.includes(location) && !isVisitedBothCities(visitedCities, locations))
+    .reduce((nearestCity, location) => (nearestCity.distance > location.distance) ? location : nearestCity)
+}
+
+const findDistances = (allLocations, distances) => {
+  const allDistances = []
+  allLocations.forEach(location => {
+    const visitedCities = [location]
+    let totalDistance = 0
+    while (allLocations.length !== visitedCities.length) {
+      const {locations, distance} = findNearestCity(distances, location, visitedCities)
+      location = locations[0] === location ? locations[1] : locations[0]
+      totalDistance += distance
+      visitedCities.push(location)
+    }
+    allDistances.push({locations: visitedCities, totalDistance})
+  })
+  return allDistances
+};
+
+
+const data = fs.readFileSync('./day9/input.txt', 'utf-8')
+  .split('\n')
+  .slice(0, -1)
+
+const distances = parseDistances(data)
+const allLocations = findAllLocations(distances);
+const totalDistances = findDistances(allLocations, distances)
+  .map(it => it.totalDistance)
+
+console.log(Math.min(...totalDistances))
